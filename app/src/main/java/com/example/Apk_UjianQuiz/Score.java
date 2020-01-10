4b7eb236eb35638fb913acf9571c5abfc08db1ea
IqbalSportProject <muhammadikbaldkc@gmail.com>
2020-01-10 20:24:42 +0700

    Review

--- app/src/main/java/com/example/Apk_UjianQuiz/Score.java
+++ app/src/main/java/com/example/Apk_UjianQuiz/Score.java
@@ -2,19 +2,126 @@ package com.example.Apk_UjianQuiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.Apk_UjianQuiz.model.QuizModel;
+import com.example.Apk_UjianQuiz.service.APIClient;
+import com.example.Apk_UjianQuiz.service.APIInterfacesRest;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Score extends AppCompatActivity {
 
-    EditText txt_view,txt_nilaiakhir;
+    TextView txt_nilaiakhir;
     Button btn_selesai, btn_ulang;
+    ArrayList<String> stock = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
 
+        txt_nilaiakhir = findViewById(R.id.txtNilaiakhir);
+        btn_selesai = findViewById(R.id.btnSelesai);
+        btn_ulang = findViewById(R.id.btnUlang);
+
+
+        Intent arr = getIntent();
+        stock = arr.getStringArrayListExtra("total");
+
+
+        callQuiz();
+
+        btn_selesai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Score.this, "TERIMA KASIH", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        btn_ulang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+
+            }
+        });
+
+    }
+
+
+    APIInterfacesRest apiInterface;
+    ProgressDialog progressDialog;
+
+    public void callQuiz() {
+
+        apiInterface = APIClient.getClient().create(APIInterfacesRest.class);
+        progressDialog = new ProgressDialog(Score.this);
+        progressDialog.setTitle("Loading");
+        progressDialog.show();
+        Call<QuizModel> call3 = apiInterface.getModel();
+        call3.enqueue(new Callback<QuizModel>() {
+            @Override
+            public void onResponse(Call<QuizModel> call, Response<QuizModel> response) {
+                progressDialog.dismiss();
+                QuizModel data = response.body();
+                //Toast.makeText(LoginActivity.this,userList.getToken().toString(),Toast.LENGTH_LONG).show();
+                if (data != null) {
+
+
+                    Integer poin = 0;
+                    Integer nilai = 0;
+
+                    for (int x = 0; x < data.getData().getSoalQuizAndroid().size(); x++) {
+
+                        ArrayList<String> hope = new ArrayList<String>(3);
+                        hope.add("4");
+                        hope.add("1");
+                        hope.add("1");
+
+                        if (stock.get(x).equalsIgnoreCase(hope.get(x))) {
+                            poin = Integer.parseInt(data.getData().getSoalQuizAndroid().get(x).getPoint());
+                            nilai += poin;
+                        }
+
+
+                        txt_nilaiakhir.setText(String.valueOf(nilai));
+
+                    }
+
+                } else {
+
+                    try {
+                        JSONObject jObjError = new JSONObject(response.errorBody().string());
+                        Toast.makeText(Score.this, jObjError.getString("message"), Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(Score.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<QuizModel> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(getApplicationContext(), "Maaf koneksi bermasalah", Toast.LENGTH_LONG).show();
+                call.cancel();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/Apk_UjianQuiz/question_activity.java
+++ app/src/main/java/com/example/Apk_UjianQuiz/question_activity.java
@@ -8,16 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.Apk_UjianQuiz.model.QuizModel;
 import com.example.Apk_UjianQuiz.service.APIClient;
 import com.example.Apk_UjianQuiz.service.APIInterfacesRest;
-import com.example.Apk_UjianQuiz.R;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,12 +27,15 @@ import retrofit2.Response;
 
 public class question_activity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView txt_Question,scoreView;
-    Button btn_A,btn_B,btn_C,btn_D;
+    TextView txt_Question, scoreView;
+    Button btn_A, btn_B, btn_C, btn_D;
     ImageView gambar1;
-
-    int sizeArr;
+    RadioGroup radioGroup;
     int i = 0;
+    int sizeArr;
+    ArrayList<String> total = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +54,11 @@ public class question_activity extends AppCompatActivity implements View.OnClick
         //imageview
         gambar1 = findViewById(R.id.gambar1);
 
+        btn_A.setId(Integer.parseInt("1"));
+        btn_B.setId(Integer.parseInt("2"));
+        btn_C.setId(Integer.parseInt("3"));
+        btn_D.setId(Integer.parseInt("4"));
+        radioGroup = findViewById(R.id.radiogrup);
         btn_A.setOnClickListener(this);
         btn_B.setOnClickListener(this);
         btn_C.setOnClickListener(this);
@@ -58,11 +68,10 @@ public class question_activity extends AppCompatActivity implements View.OnClick
     }
 
 
-
-
     APIInterfacesRest apiInterface;
     ProgressDialog progressDialog;
-    public void callquiz(){
+
+    public void callquiz() {
 
         apiInterface = APIClient.getClient().create(APIInterfacesRest.class);
         progressDialog = new ProgressDialog(question_activity.this);
@@ -76,31 +85,34 @@ public class question_activity extends AppCompatActivity implements View.OnClick
                 QuizModel data = response.body();
                 //Toast.makeText(LoginActivity.this,userList.getToken().toString(),Toast.LENGTH_LONG).show();
 
-                if (data !=null) {
+                if (data != null) {
                     sizeArr = data.getData().getSoalQuizAndroid().size();
 
-                    if (i < sizeArr){
+                    if (i < sizeArr) {
+                        if (data.getData().getSoalQuizAndroid().get(i).getJenisPertanyaan().equalsIgnoreCase("gambar")) {
+
+                            txt_Question.setText(data.getData().getSoalQuizAndroid().get(i).getPertanyaan());
+                            btn_A.setText(data.getData().getSoalQuizAndroid().get(i).getA());
+                            btn_B.setText(data.getData().getSoalQuizAndroid().get(i).getB());
+                            btn_C.setText(data.getData().getSoalQuizAndroid().get(i).getC());
+                            btn_D.setText(data.getData().getSoalQuizAndroid().get(i).getD());
 
 
-                        txt_Question.setText(data.getData().getSoalQuizAndroid().get(i).getPertanyaan());
-                    btn_A.setText(data.getData().getSoalQuizAndroid().get(i).getA());
-                    btn_B.setText(data.getData().getSoalQuizAndroid().get(i).getB());
-                    btn_C.setText(data.getData().getSoalQuizAndroid().get(i).getC());
-                    btn_D.setText(data.getData().getSoalQuizAndroid().get(i).getD());
+                            String image = data.getData().getSoalQuizAndroid().get(i).getGambar();
+                            Picasso.get().load(image).into(gambar1);
+                        } else{
+                            txt_Question.setText(data.getData().getSoalQuizAndroid().get(i).getPertanyaan());
+                            btn_A.setText(data.getData().getSoalQuizAndroid().get(i).getA());
+                            btn_B.setText(data.getData().getSoalQuizAndroid().get(i).getB());
+                            btn_C.setText(data.getData().getSoalQuizAndroid().get(i).getC());
+                            btn_D.setText(data.getData().getSoalQuizAndroid().get(i).getD());
 
+                        }
+                        i++;
 
-                    String image = data.getData().getSoalQuizAndroid().get(i).getGambar();
-                    Picasso.get().load(image).into(gambar1);
                     }
-                    else if( i == sizeArr){
-                        Intent score = new Intent(question_activity.this, Score.class);
-                        startActivity(score);
-                }
-                    i++;
 
-
-
-                }else{
+                } else {
 
                     try {
                         JSONObject jObjError = new JSONObject(response.errorBody().string());
@@ -114,18 +126,25 @@ public class question_activity extends AppCompatActivity implements View.OnClick
             @Override
             public void onFailure(Call<QuizModel> call, Throwable t) {
                 progressDialog.dismiss();
-                Toast.makeText(getApplicationContext(),"Maaf koneksi bermasalah",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Maaf koneksi bermasalah", Toast.LENGTH_LONG).show();
                 call.cancel();
             }
         });
-
-
 
 
     }
 
     @Override
     public void onClick(View v) {
+        total.add(String.valueOf(v.getId()));
+        Toast.makeText(question_activity.this, String.valueOf(total), Toast.LENGTH_LONG).show();
         callquiz();
+        if (i == 3) {
+
+            Intent i = new Intent(question_activity.this, Score.class);
+            i.putStringArrayListExtra("total", total);
+            startActivity(i);
+            finish();
+        }
     }
 }
