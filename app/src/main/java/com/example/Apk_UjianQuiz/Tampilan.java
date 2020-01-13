67a5932bef38dfacecf4a8a4579e9a8028f6a433
IqbalSportProject <muhammadikbaldkc@gmail.com>
2020-01-13 09:42:16 +0700

    Review

--- app/src/main/java/com/example/Apk_UjianQuiz/Tampilan.java
+++ app/src/main/java/com/example/Apk_UjianQuiz/Tampilan.java
@@ -1,22 +1,31 @@
 package com.example.Apk_UjianQuiz;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Tampilan extends AppCompatActivity {
-
-    ImageView onClick;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tampilan);
-    }
-    public void onClick(View v){
-        Intent intent = new Intent(Tampilan.this,question_activity.class);
+
+        img = findViewById(R.id.btnImg);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Tampilan.this, question_activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
